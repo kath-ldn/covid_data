771c5d6d81e787256bce81c0b2a3408112ce8949
kath <katharineturner3@gmail.com>
2021-06-17 13:11:06 +0100

    V2.0 Final Version to Submit

--- readme.md
+++ readme.md
@@ -1,27 +1,27 @@
-# Code in Place Final Project
-## Basic version using Matplotlib
+# Code in Place Final Project: COVID19 Data Analysis
 
 ## About
 
-This is my final project for the Stanford Code In Place course. I am exploring data science as this isn't something I've done as much of as I've mainly been working with JavaScript prior to this.
+This is my final project for the Stanford Code In Place Python course! I am exploring data science because this isn't 
+something I hadn't previously done as much of. I had mainly been working with JavaScript/the front end prior to this course.
 
-I have chosen to analyse some COVID19 vaccine data from the UK. I may expand this to global/cases per day.
-Think about using github (large dataset)? User can input their country or random, or overlay some other data.
+This program analyses how many first and second dose COVID19 vaccines have been given. This could be expanded to other
+countries/global vaccines, with the right data.
 
 ### Demo
 
-Space to include a screenshot if applicable.
+![Screenshot of Bar Chart showing COVID19 data](./screenshot-17-Jun.png?raw=true)
 
 ### Built with
 
 * Python
-* Seaborn
+* MatPlotLib
 
 ## Prerequisites & Installation
 
 * the latest version of python (3.9)
-* seaborn (and dependency packages)
-* I used a conda environment, but this is optional
+* MatPlotLib (and dependency packages)
+* I used a Conda environment, but this is optional
 
 ## Usage
 
@@ -29,7 +29,7 @@ Load the project in your IDE(I used PyCharm) and run to see the data.
 
 ## Licence
 
-Distributed under the [MIT](https://choosealicense.com/licenses/mit/) licence.
+Program distributed under the [MIT](https://choosealicense.com/licenses/mit/) licence.
 
 All data is property of the UK government and available at https://coronavirus.data.gov.uk/details/vaccinations.
 
@@ -38,13 +38,16 @@ Kath Turner - @kath_ldn - katharineturner3@gmail.com
 
 ### Known Issues
 
-* doesn't start from zero as uk vac programme started on 08 december and started publishing in jan.
-* counted by date of publish not date of vaccine.
+* The data doesn't start from zero because although the UK vaccination programme started on 08 December 2020,
+  publication of the data started in January 2021. The data shows the number of vaccines by publication date,
+  not by date they are given. Therefore it may not precisely reflect numbers given on each day.
+* I have not tested how the program would run if data was incomplete, and the dates on each did not match each other.
+  This would likely require additional code to work.
 
 ## Acknowledgements
 
 * Miniconda
-* Seaborn
-* UK Government for data
+* MatPlotLib
+* UK Government for the data
 
 
--- vaccine_analysis.py
+++ vaccine_analysis.py
@@ -1,58 +1,54 @@
 # for loading json files
-import json
-import numpy as np
+import requests
 import matplotlib.pyplot as plt
 import datetime
 import matplotlib.dates as mdates
 from datetime import datetime
 
-#To do: rationalise - repeat a lot e.g. pop and reverse
+FIRST_VAC_API = requests.get(str("https://coronavirus.data.gov.uk/api/v1/data?filters=areaType=overview&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newPeopleVaccinatedFirstDoseByPublishDate%22:%22newPeopleVaccinatedFirstDoseByPublishDate%22,%22cumPeopleVaccinatedFirstDoseByPublishDate%22:%22cumPeopleVaccinatedFirstDoseByPublishDate%22%7D&format=json"))
+SECOND_VAC_API = requests.get(str("https://coronavirus.data.gov.uk/api/v1/data?filters=areaType=overview&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newPeopleVaccinatedSecondDoseByPublishDate%22:%22newPeopleVaccinatedSecondDoseByPublishDate%22,%22cumPeopleVaccinatedSecondDoseByPublishDate%22:%22cumPeopleVaccinatedSecondDoseByPublishDate%22%7D&format=json"))
 
 def main():
-    first_vac = get_vaccine_data("First")
-    second_vac = get_vaccine_data(("Second"))
-    first_keys = list(first_vac.keys())
-    second_keys = list(first_vac.keys())
-    first_keys.reverse()
-    first_keys.pop(0)
-    second_keys.reverse()
-    second_keys.pop(0)
-    if first_keys == second_keys:
-        first_values = list(first_vac.values())
-        second_values = list(second_vac.values())
-        first_values.reverse()
-        first_values.pop(0)
-        second_values.reverse()
-        second_values.pop(0)
-        width = 0.35
-        fig, axs = plt.subplots(constrained_layout=True)
-        axs.bar(first_keys, first_values, width, label="First Dose")
-        axs.bar(first_keys, second_values, width, bottom=first_values, label="Second Dose")
-        axs.set_ylabel("Doses (in 1000s)")
-        axs.set_title("COVID19 Vaccines given in the UK")
-        axs.legend()
-        locator = mdates.AutoDateLocator()
-        formatter = mdates.ConciseDateFormatter(locator)
-        axs.xaxis.set_major_locator(locator)
-        axs.xaxis.set_major_formatter(formatter)
-        plt.show()
-
-    else:
-        print("Error: dates across files do not match.")
+    first_vac_dates, second_vac_dates, first_vac_num, second_vac_num = get_data()
+    draw_chart(first_vac_dates, second_vac_dates, first_vac_num, second_vac_num)
 
 
-def format_date(str_date):
+def get_data():
+    first_vac = format_api_data((FIRST_VAC_API.json()), "First")
+    second_vac = format_api_data((SECOND_VAC_API.json()), "Second")
+    first_vac_dates = list(first_vac.keys())
+    second_vac_dates = list(second_vac.keys())
+    first_vac_num = list(first_vac.values())
+    second_vac_num = list(second_vac.values())
+    return first_vac_dates, second_vac_dates, first_vac_num, second_vac_num
+
+
+def draw_chart(first_vac_dates, second_vac_dates, first_vac_num, second_vac_num):
+    width = 0.6
+    fig, axs = plt.subplots()
+    axs.bar(first_vac_dates, first_vac_num, width, label="First Dose")
+    axs.bar(first_vac_dates, second_vac_num, width, bottom=first_vac_num, label="Second Dose")
+    axs.set_ylabel("Doses (in 1000s)")
+    axs.set_title("Number of People Vaccinated Against COVID19 In The UK")
+    axs.legend()
+    locator = mdates.AutoDateLocator()
+    formatter = mdates.ConciseDateFormatter(locator)
+    axs.xaxis.set_major_locator(locator)
+    axs.xaxis.set_major_formatter(formatter)
+    plt.show()
+
+
+def change_date_type(str_date):
     date_obj = datetime.strptime(str_date, '%Y-%m-%d')
     date = date_obj.date()
     return date
 
 
-def get_vaccine_data(vac_num):
-    file = json.load(open(vac_num + '-vac.json'))
+def format_api_data(file, vac_num):
     new_file = {}
     for item in file["data"]:
         str_date = item["date"]
-        date = format_date(str_date)
+        date = change_date_type(str_date)
         if item["newPeopleVaccinated" + vac_num + "DoseByPublishDate"] is not None:
             new_file[date] = int(item["newPeopleVaccinated" + vac_num + "DoseByPublishDate"]) / 1000
         elif item["newPeopleVaccinated" + vac_num + "DoseByPublishDate"] is None:
